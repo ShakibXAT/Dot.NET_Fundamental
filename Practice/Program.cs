51e2e4c9295c23d43cac9665bd7c71227d252365
Shakib Ahamed Tushar <shakibxat@gmail.com>
2021-07-16 03:11:06 +0600

    DotNet Loops

---
+++ Practice/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //  var names = new List<String> {"Shakib", "Shameem", "Shawan"};
+           // names.Add("Hati");
+           // names.Remove("Shameem");
+           //
+           //  foreach (var name in names)
+           //  {
+           //      Console.WriteLine($"{name.ToUpper()} ");
+           //  }
+           //  Console.WriteLine($"my name is {names[0]}");
+           //  Console.WriteLine($"The list has {names.Count} values ");
+           //  Console.WriteLine(names.IndexOf("Shawan"));
+           //  
+
+           var fibonacci = new List<int>{1,1};
+           while (fibonacci.Count < 20)
+           {
+               var fibonacciNumber = fibonacci[fibonacci.Count - 1];
+               var fibonacciNumber2 = fibonacci[fibonacci.Count - 2];
+               fibonacci.Add(fibonacciNumber+fibonacciNumber2);
+               
+           }
+
+           
+           foreach (var fibo in fibonacci)
+           {
+              
+               Console.WriteLine(fibo);
+           }
+
+        }
+    }
+}
